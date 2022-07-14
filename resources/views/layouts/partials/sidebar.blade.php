c67e4853891f35217207ccf5c56ae2d2eae3d134
unknown <neet693@gmail.com>
2022-07-14 14:58:13 +0700

    Sidebar fix

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -2,11 +2,11 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
                 <div class="sidebar-brand-icon">
                     <img src="{{ asset('img/logo-gram-sky.png') }}" alt="Logo" style="width: 50px">
                 </div>
-                <div class="sidebar-brand-text mx-3">SKYPEG</div>
+                <div class="sidebar-brand-text mx-3">SKY SIMPEG</div>
             </a>
 
             <!-- Divider -->
